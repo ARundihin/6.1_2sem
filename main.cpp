5fcc1b96b05d1ed7e8483c05b9b5e1d7ecb3ff6d
ARundihin <142354563+ARundihin@users.noreply.github.com>
2024-05-27 21:43:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include "coin.hpp"
+#include "coldWeapon.hpp"
+#include "painting.hpp"
+#include "statuette.hpp"
+#include "photograph.hpp"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Coin coin("Монета", 2019, 1.0, 2000, 2020);
+    ColdWeapon weapon("Холодное оружие", 1800, "XIX-XX век");
+    Painting painting("Картина", 1900, "Пейзаж", "Иван Иванов");
+    Statuette statuette("Статуэтка", 1700, "Богиня", "Петр Петров");
+    Photograph photograph("Фотография", 2005, "Портрет");
+
+    coin.Print();
+    cout << endl;
+    weapon.Print();
+    cout << endl;
+    painting.Print();
+    cout << endl;
+    statuette.Print();
+    cout << endl;
+    photograph.Print();
+
+    return 0;
+}
